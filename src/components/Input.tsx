47e96ca063956170534cc2447708cab9132242ac
Yuri Ruas <yuriruas.dev@gmail.com>
2023-12-11 14:25:15 -0300

    sidebar: pattern de composição

---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+import { Search } from 'lucide-react'
+import { ComponentProps } from 'react'
+
+type InputPrefixProps = ComponentProps<'div'>
+
+type InputControlProps = ComponentProps<'input'>
+
+type InputRootProps = ComponentProps<'div'>
+
+export function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+export function Control(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    ></div>
+  )
+}
