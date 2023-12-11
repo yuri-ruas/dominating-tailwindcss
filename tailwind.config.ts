db87272a18b184e0de9f783f345a2465e370672a
Yuri Ruas <yuriruas.dev@gmail.com>
2023-12-11 16:00:50 -0300

    formulário: campos do formulário

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,11 @@ const config: Config = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
 
       maxWidth: {
         app: '700px',
-      },
-
-      colors: {
-        diego: '#333',
       },
     },
   },
