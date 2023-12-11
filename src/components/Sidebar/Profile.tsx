65be8ee2808fa286850d31ceed0bd317e00b3cb5
Yuri Ruas <yuriruas.dev@gmail.com>
2023-12-11 14:03:58 -0300

    sidebar: perfil do usuário

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/yuri-ruas.png"
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">Yuri Ruas</span>
+        <span className="truncate text-sm text-zinc-500">
+          yuriruas.dev@gmail.com
+        </span>
+      </div>
+
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
